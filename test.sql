5ff8048fdb350169089024618998394599873569
Suyog Darokar <84698669+suyog-24@users.noreply.github.com>
2022-01-06 23:29:47 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Student(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Suyog', 01, 'Warud', 9543115477, 'XYZ20010');
+INSERT INTO studentData values('Ajay', 02, 'Hyderabad', 9632145788, 'XYZ020202');
+INSERT INTO studentData VALUES('Rahul', 03, 'Banglore', 7706534586, 'STR030203');
